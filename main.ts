170a641fd95a9162b26d4d3160d560a476542289
Cryptid <cryptidbleh@icloud.com>
2025-12-26 23:02:25 +0100

    shitty untested webchat stuff yippie

--- main.ts
+++ main.ts
@@ -26,6 +26,8 @@ let KeyboardVisible = false
 let Write_icon: Sprite = null
 let Library_icon: Sprite = null
 let RadioValueQueue: string[] = []
+let IncomingMessageChunks: string[] = []
+let ExpectedChunks = 0
 let xCell_Icon: Sprite = null
 let outline: Sprite = null
 let Mouse_Cursor: Sprite = null
@@ -367,9 +369,7 @@ function MouseClick(button: number) {
                     WebChatMessages.shift();
                 } 
                 KeyboardVisible = false
-                radio.sendString(
-                    encrypt("WebChat", parseInt(RoomCode)),
-                )
+                sendWebChatMessage(Username, WEBmessage)
             } else if (Mouse_Cursor.x > 0 && Mouse_Cursor.x < 148 && Mouse_Cursor.y > 92 && Mouse_Cursor.y < 105 && button == 1) {
                 KeyboardVisible = true
                 WEBmessage = game.askForString("Type your message here", 36)
@@ -388,36 +388,109 @@ radio.onReceivedString(function (name: string) {
     RadioValueQueue.push(name)
 })
 
+radio.onReceivedValue(function (name: string, value: number) {
+    handleWebChatChunk(name, value)
+})
+
 function processRadioQueue() {
     if (App_Open == "Web Chat" && KeyboardVisible == false) {
-        for (let i = 0; i < RadioValueQueue.length; i++) {
+        while (RadioValueQueue.length > 0) {
             let message = RadioValueQueue.shift()
-            // let decryptemetadata = decrypt(DecodeFromNumber(message[1]).toString(), Number(RoomCode))
-            //     if (decrypt(message[0], Number(RoomCode)) == "WebChat") {
-            //         const metadata = decryptemetadata.split("~")
-            //     if (metadata[1] == RoomCode) {
-            //         let verified = ""
-            //         if (metadata[2] == null) { } else {
-            //             // nobody other than system is verified yet cuz i've yet to crack accessing the serial number
-            //             verified = "(Verified)"
-            //         }
-            //         WebChatMessages[7] = miniMenu.createMenuItem(metadata[0] + " " + verified)
-            //         WebChatMessages.push(miniMenu.createMenuItem(metadata[3]))
-            //         WebChatMessages.push(miniMenu.createMenuItem(Temp))
-            //         while (WebChatMessages.length > 8) {
-            //             WebChatMessages.shift();
-            //         } 
-            //     }
-            // }
+            if (message) {
+                // Message format: "username|message"
+                let parts = message.split("|")
+                if (parts.length >= 2) {
+                    let senderName = parts[0]
+                    let messageText = parts.slice(1).join("|")
+                    
+                    WebChatMessages[7] = miniMenu.createMenuItem(senderName)
+                    WebChatMessages.push(miniMenu.createMenuItem(messageText))
+                    WebChatMessages.push(miniMenu.createMenuItem(Temp))
+                    while (WebChatMessages.length > 8) {
+                        WebChatMessages.shift();
+                    }
+                    
+                    ListMenuGUI.destroy()
+                    WebChatMessages[7] = miniMenu.createMenuItem(Temp)
+                    ListMenuGUI = miniMenu.createMenuFromArray(WebChatMessages)
+                    ListMenuGUI.setDimensions(160, 97)
+                    ListMenuGUI.setButtonEventsEnabled(false)
+                    ListMenuGUI.setPosition(80, 58)
+                    ListMenuGUI.selectedIndex = 7
+                    ListMenuGUI.z = -30
+                }
+            }
         }
-        ListMenuGUI.destroy()
-        WebChatMessages[7] = miniMenu.createMenuItem(Temp)
-        ListMenuGUI = miniMenu.createMenuFromArray(WebChatMessages)
-        ListMenuGUI.setDimensions(160, 97)
-        ListMenuGUI.setButtonEventsEnabled(false)
-        ListMenuGUI.setPosition(80, 58)
-        ListMenuGUI.selectedIndex = 7
-        ListMenuGUI.z = -30
+    }
+}
+
+function sendWebChatMessage(username: string, message: string) {
+    let fullMessage = username + "|" + message
+    let chunkSize = 18
+    let totalChunks = Math.ceil(fullMessage.length / chunkSize)
+
+    radio.sendValue("WCH", totalChunks * 1000 + parseInt(RoomCode.substr(0, 3)))
+    pause(50)
+
+    for (let i = 0; i < totalChunks; i++) {
+        let start = i * chunkSize
+        let end = Math.min(start + chunkSize, fullMessage.length)
+        let chunk = fullMessage.substr(start, end - start)
+
+        let chunkValues = encodeStringToValues(chunk)
+        for (let j = 0; j < chunkValues.length; j++) {
+            let name = "WC" + String.fromCharCode(48 + i) + String.fromCharCode(48 + j)
+            radio.sendValue(name, chunkValues[j])
+            pause(30)
+        }
+    }
+    
+    radio.sendValue("WCE", parseInt(RoomCode.substr(0, 4)))
+}
+
+function encodeStringToValues(text: string): number[] {
+    let values: number[] = []
+    for (let i = 0; i < text.length; i += 3) {
+        let val = 0
+        for (let j = 0; j < 3 && i + j < text.length; j++) {
+            val = val * 256 + text.charCodeAt(i + j)
+        }
+        values.push(val)
+    }
+    return values
+}
+
+function decodeValueToString(value: number, length: number): string {
+    let result = ""
+    let chars: number[] = []
+    while (value > 0) {
+        chars.unshift(value % 256)
+        value = Math.floor(value / 256)
+    }
+    for (let i = 0; i < chars.length && i < length; i++) {
+        result += String.fromCharCode(chars[i])
+    }
+    return result
+}
+
+function handleWebChatChunk(name: string, value: number) {
+    if (name.substr(0, 2) == "WC") {
+        if (name == "WCH") {
+            let roomCheck = value % 1000
+            if (roomCheck == parseInt(RoomCode.substr(0, 3))) {
+                IncomingMessageChunks = []
+                ExpectedChunks = Math.floor(value / 1000)
+            }
+        } else if (name == "WCE") {
+            if (value == parseInt(RoomCode.substr(0, 4)) && IncomingMessageChunks.length > 0) {
+                let fullMessage = IncomingMessageChunks.join("")
+                RadioValueQueue.push(fullMessage)
+                IncomingMessageChunks = []
+            }
+        } else if (name.length == 4) {
+            let decoded = decodeValueToString(value, 3)
+            IncomingMessageChunks.push(decoded)
+        }
     }
 }
 
